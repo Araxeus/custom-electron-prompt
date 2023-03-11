350dab1c55a5e0ab767724de92922d4e5b32c83f
Araxeus <78568641+Araxeus@users.noreply.github.com>
2023-03-11 14:01:48 +0200

    apply rome format

--- example/custom-titlebar.js
+++ example/custom-titlebar.js
@@ -7,7 +7,7 @@ module.exports = () => {
 		backgroundColor: customTitlebar.Color.fromHex("#050505"),
 		minimizable: false,
 		maximizable: false,
-		menu: null
+		menu: null,
 	});
 	const mainStyle = document.querySelector("#container").style;
 	mainStyle.width = "100%";
